c24ca8521310afd9640f579de6dbbae919f26ec3
Zak Hamilton <z.hamilton.zh@gmail.com>
2022-07-15 21:43:49 +1000

    Adds multithreaded example.
    Still needs thread pooling to work with arbitrarily large iterations

--- book_stats/src/lib.rs
+++ book_stats/src/lib.rs
@@ -1,26 +1,36 @@
+use std::fmt;
+use std::sync;
+use std::thread;
+
 mod analysis;
 mod filesystem;
 mod networking;
 
 const TEST_BOOK: &str = "sherlock_holmes.txt";
 
-pub fn analyse_book() {
-    let book = get_book();
-
-    let word_count = analysis::get_word_count(&book);
-    println!("Book has {} words", word_count);
-
-    let line_count = analysis::get_line_count(&book);
-    println!("Book has {} lines", line_count);
-
-    let (most_common_word, count) = analysis::get_most_common_word(&book);
-    println!(
-        "Book has most common word: {}, occurring {} times",
-        most_common_word, count
-    );
+#[derive(Debug, Default)]
+pub struct BookStats {
+    name: String,
+    word_count: usize,
+    line_count: usize,
+    most_common_word: (String, usize),
 }
 
-fn get_book() -> String {
+impl fmt::Display for BookStats {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} stats:\nWord count: {}, line count: {}, most common word: {} ({})",
+            self.name,
+            self.word_count,
+            self.line_count,
+            self.most_common_word.0,
+            self.most_common_word.1
+        )
+    }
+}
+
+pub fn get_book() -> String {
     if filesystem::book_exists(TEST_BOOK) {
         filesystem::read_book_from_disk(TEST_BOOK)
     } else {
@@ -29,5 +39,47 @@ fn get_book() -> String {
 
         filesystem::write_book_to_disk(TEST_BOOK, &book);
         book
+    }
+}
+
+pub fn analyse_book(book: &str) -> BookStats {
+    let word_count = analysis::get_word_count(book);
+    let line_count = analysis::get_line_count(book);
+    let most_common_word = analysis::get_most_common_word(book);
+
+    BookStats {
+        name: String::from(TEST_BOOK.strip_suffix(".txt").unwrap()),
+        word_count,
+        line_count,
+        most_common_word,
+    }
+}
+
+pub fn analyse_book_threaded(book: &str) -> BookStats {
+    let (wc_tx, wc_rx) = sync::mpsc::channel();
+    let (lc_tx, lc_rx) = sync::mpsc::channel();
+    let (mcw_tx, mcw_rx) = sync::mpsc::channel();
+
+    thread::scope(|s| {
+        s.spawn(move || {
+            wc_tx.send(analysis::get_word_count(book)).unwrap();
+        });
+        s.spawn(move || {
+            lc_tx.send(analysis::get_line_count(book)).unwrap();
+        });
+        s.spawn(move || {
+            mcw_tx.send(analysis::get_most_common_word(book)).unwrap();
+        });
+    });
+
+    let word_count = wc_rx.recv().unwrap();
+    let line_count = lc_rx.recv().unwrap();
+    let most_common_word = mcw_rx.recv().unwrap();
+
+    BookStats {
+        name: String::from(TEST_BOOK.strip_suffix(".txt").unwrap()),
+        word_count,
+        line_count,
+        most_common_word,
     }
 }
--- book_stats/src/main.rs
+++ book_stats/src/main.rs
@@ -1,3 +1,35 @@
+use std::thread;
+use std::time;
+
 fn main() {
-    book_stats::analyse_book();
+    let book = book_stats::get_book();
+
+    let now = time::Instant::now();
+    (0..5).for_each(|_| {
+        book_stats::analyse_book(&book);
+    });
+    let after = time::Instant::now();
+
+    println!("Sync: {}", (after - now).as_secs_f64());
+
+    let now = time::Instant::now();
+    (0..5).for_each(|_| {
+        book_stats::analyse_book_threaded(&book);
+    });
+    let after = time::Instant::now();
+
+    println!("Single threaded: {}", (after - now).as_secs_f64());
+
+    let book_ref = &book;
+    let now = time::Instant::now();
+    thread::scope(|s| {
+        (0..500).for_each(|_| {
+            s.spawn(move || {
+                book_stats::analyse_book_threaded(book_ref);
+            });
+        });
+    });
+    let after = time::Instant::now();
+
+    println!("Double threaded: {}", (after - now).as_secs_f64());
 }
